c9b647f73dbdcffabd27e3daf0526d1219abf46d
alaricli <alaricli@outlook.com>
2024-11-04 14:38:29 -0800

    new changes

---
+++ src/app/account/page.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export default function AccountPage() {
+  useEffect(() => {
+    window.location.href = 'https://www.scentsaver.co/account';
+  }, []);
+
+  return (
+    <div>
+      <p>Redirecting to your account...</p>
+    </div>
+  );
+}
