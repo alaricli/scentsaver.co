a3981e2ba226b89c6678a95ad6d4c48e6652267b
alaricli <alaricli@outlook.com>
2024-11-04 14:27:56 -0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,32 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const SHOPIFY_ACCOUNT_BASE_URL = 'http://scentsaver.co';
+const SHOPIFY_DOMAIN = 'scentsaver.co';
+const SHOPIFY_ACCOUNT_BASE_URL = `http://${SHOPIFY_DOMAIN}`;
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  const { pathname, host } = request.nextUrl;
 
-  // Construct the full Shopify URL
-  const searchParams = request.nextUrl.searchParams.toString();
-  const hash = request.nextUrl.hash;
+  // Only redirect if we're not already on the Shopify domain
+  if (!host.includes(SHOPIFY_DOMAIN)) {
+    const searchParams = request.nextUrl.searchParams.toString();
+    const hash = request.nextUrl.hash;
 
-  let shopifyUrl = `${SHOPIFY_ACCOUNT_BASE_URL}${pathname}`;
-  if (searchParams) {
-    shopifyUrl += `?${searchParams}`;
+    let shopifyUrl = `${SHOPIFY_ACCOUNT_BASE_URL}${pathname}`;
+    if (searchParams) {
+      shopifyUrl += `?${searchParams}`;
+    }
+    if (hash) {
+      shopifyUrl += hash;
+    }
+
+    return NextResponse.redirect(shopifyUrl);
   }
-  if (hash) {
-    shopifyUrl += hash;
-  }
 
-  // Perform the redirect
-  return NextResponse.redirect(shopifyUrl);
+  // If we're already on the Shopify domain, just continue normally
+  return NextResponse.next();
 }
 
-// Configure middleware to match any path starting with /account
 export const config = {
   matcher: '/account/:path*',
 };
